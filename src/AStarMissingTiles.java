3e1e2167d978013d0ea1269e94c354ca2db53ba6
einhause <55774992+einhause@users.noreply.github.com>
2020-04-07 20:11:56 -0400

    Update AStarMissingTiles.java

--- src/AStarMissingTiles.java
+++ src/AStarMissingTiles.java
@@ -3,29 +3,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * For this assignment, you will need the puzzle.jar file and its documentation (docs.zip) 
- * from homework 4, and the Examples.java will be useful as well.
- *
- * In Java, implement any search algorithm that we saw in class to solve sliding tile
- * puzzles, such as the 8 puzzle, 15 puzzle, etc.
- *
- * You must not change the name, parameters, or return type of the solver method.
- * You are free to add as many helper methods as you find useful.
- *
- * You are free to use the MinHeapPQ class that is in the puzzle.jar (see its documentation).
- * It supports changing the priority of elements that are in the PQ, whereas the PQ implementation
- * in the Java API does not support that operation.
- *
- * Grading:
- * Your grade will be in the interval [0, 100] if you implement an algorithm that is guaranteed
- * to find the optimal path.  Otherwise, if you implement an algorithm that is not
- * guaranteed to find the optimal path (e.g., DFS), then your grade will be in the interval [0, 85]
- * (i.e., you lose 15 points for the non-optimal algorithms).
- *
- * If your code doesn't compile, then your grade will be in the interval [0, 60] depending upon the severity
- * of the syntax errors.  i.e., make sure your code compiles (you lose at least 40 points if it doesn't.
- *
- * After completing the solver method, write code to demonstrate that it works in the Homework5Main class.
  *
  * @author Eric Einhaus
  *
@@ -84,11 +61,11 @@ public class Homework5 {
 			//and keep track of its backpointer in this form -> (successor, polledLowestPriorityPuzzle)
 			ArrayList<SlidingTilePuzzle> successors = puzzleWithLowestPri.getSuccessors();
 			for (SlidingTilePuzzle s : successors) {
-				//Let f’ = g(n') + h(n') = g(n) + cost(n,n') + h(n')
+				//Let fâ€™ = g(n') + h(n') = g(n) + cost(n,n') + h(n')
 				int fPrime = priority + 1 + numOfMisplacedTiles(s, solvedSlidingTilePuzzle, rows, columns);
-				//If n' not seen before, or n' previously expanded with f(n')>f’, or n' currently in PQ with f(n')>f’
+				//If n' not seen before, or n' previously expanded with f(n')>fâ€™, or n' currently in PQ with f(n')>fâ€™
 				if (!fValues.containsKey(s) || (fValues.containsKey(s) && fValues.get(s) > fPrime) || (pq.inPQ(s) && fValues.get(s) > fPrime)) {
-					//Then Place/promote n' on priority queue with priority f’ and update V to include (state=n', f ’, BackPtr=n).
+					//Then Place/promote n' on priority queue with priority fâ€™ and update V to include (state=n', f â€™, BackPtr=n).
 					pq.offer(s, fPrime);
 					fValues.put(s, fPrime);
 					back.put(s, puzzleWithLowestPri);
